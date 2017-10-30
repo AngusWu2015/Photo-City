78ee1feed8ffc6435d0d4f17a509107241c7a24d
Andy.Wu <andy62210@hotmail.com>
2017-10-30 13:50:37 +0800

    first commit

---
+++ photo-city/Controller/MapVC.swift
@@ -0,0 +1,26 @@
+//
+//  MapVC.swift
+
+//  photo-city
+//
+//  Created by AndyWu on 2017/10/30.
+//  Copyright © 2017年 AndyWu. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
